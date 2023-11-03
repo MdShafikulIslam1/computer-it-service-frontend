1232c2f0491798dfe36fde37f760d5f97d3a1900
shafikul-islam <179398ssshofik@gmail.com>
2023-11-03 14:13:00 +0600

    added fap section on faq page

--- src/app/(public)/faq/page.tsx
+++ src/app/(public)/faq/page.tsx
@@ -1,30 +1,17 @@
-// import FAQPart from '@/components/HomePage/FAQ/FAQPart';
-// import React from 'react';
+'use client'
+import TextWithUnderLine from "@/components/Divider/Divider";
+import FAQPage from "@/components/HomePage/FAQ/FAQ";
+import Overlay from "@/components/Overlay/Overlay";
 
-// const FAQPage = () => {
-//     return (
-      
-//           <div className="w-full lg:w-1/2 py-2 px-6">
-//         <h3 className="text-blue-700 font-semibold text-2xl my-2">FAQs ----</h3>
-//         <h1>Frequently Asked Question</h1>
-       
-//           <FAQPart />
-  
-//       </div>   
-    
-//     );
-// };
-
-// export default FAQPage;
-
-
-
-const FAQPage = () => {
+const FAQPages = () => {
     return (
-        <div>
-           <h1>this is faq page</h1> 
+       <>
+       <Overlay heading="FAQ" currentPageTitle="FAQs"/>
+       <div>
+         <FAQPage/>
         </div>
+       </>
     );
 };
 
-export default FAQPage;
+export default FAQPages;
